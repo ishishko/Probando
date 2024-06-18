f9697ee65214e7fa6aefc2ac3e2564f7b09640ba
Ignacio Shishko <shishkoignacio@gmail.com>
2024-06-18 06:59:55 -0300

    Curso python 02

--- seccion11_encapsulamiento/02-02-00-Encapsulamiento-UP.py
+++ seccion11_encapsulamiento/02-02-00-Encapsulamiento-UP.py
@@ -5,9 +5,19 @@ class Persona:
         self.apellido = apellido
         self.edad = edad
 
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
     def mostrar_detalle(self):
         print(f'Persona: {self._nombre} {self.apellido} {self.edad}')
 
 persona1 = Persona('Juan', 'Perez', 28)
-persona1._nombre = 'Juan Carlos'
+persona1.mostrar_detalle()
+
+persona1.nombre = 'Juan Carlos'
 persona1.mostrar_detalle()
